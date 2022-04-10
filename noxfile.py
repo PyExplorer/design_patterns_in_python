5b628e84dda94dbac0cf5bab9b481e078e3ffda7
PyExplorer <stgmont@gmail.com>
2022-04-10 14:24:09 +0300

    nox only for py3.8

--- noxfile.py
+++ noxfile.py
@@ -21,7 +21,8 @@ except ImportError:
 
 
 package = "design_patterns_in_python"
-python_versions = ["3.10", "3.9", "3.8", "3.7"]
+# python_versions = ["3.10", "3.9", "3.8", "3.7"]
+python_versions = ["3.8"]
 nox.needs_version = ">= 2021.6.6"
 nox.options.sessions = (
     "pre-commit",
