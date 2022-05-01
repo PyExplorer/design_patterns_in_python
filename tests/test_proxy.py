e084d15c539fb35f3b998916b40f97d4e461c26f
PyExplorer <stgmont@gmail.com>
2022-05-01 20:28:19 +0300

    proxy, add test for ResponsiblePerson

--- tests/test_proxy.py
+++ tests/test_proxy.py
@@ -3,6 +3,7 @@
 from src.design_patterns_in_python.proxy import Person
 from src.design_patterns_in_python.proxy import ResponsiblePerson
 
+
 class TestPerson:
     def test_drink(self):
         p = Person(10)
@@ -16,3 +17,22 @@ class TestPerson:
         p = Person(10)
         assert p.drink_and_drive() == f"Age {p.age}: driving while drunk"
 
+
+class TestResponsiblePerson:
+    def test_drink(self):
+        rp = ResponsiblePerson(Person(10))
+        assert rp.drink() == f"Age {rp.age}: no drinking: too young"
+        rp.age = 20
+        assert rp.drink() == f"Age {rp.age}: drinking"
+
+    def test_drive(self):
+        rp = ResponsiblePerson(Person(10))
+        assert rp.drive() == f"Age {rp.age}: no driving: too young"
+        rp.age = 20
+        assert rp.drive() == f"Age {rp.age}: driving"
+
+    def test_drink_and_drive(self):
+        rp = ResponsiblePerson(Person(10))
+        assert rp.drink_and_drive() == f"Age {rp.age}: drink and drive: dead"
+        rp.age = 20
+        assert rp.drink_and_drive() == f"Age {rp.age}: drink and drive: dead"
