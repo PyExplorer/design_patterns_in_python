aa83941514a5d05d41af9b63e30b6598b8f93936
PyExplorer <stgmont@gmail.com>
2022-05-02 18:05:26 +0300

    chain of responsibility start

---
+++ src/design_patterns_in_python/chain_of_responsibility.py
@@ -0,0 +1,6 @@
+"""Chain fo responsibility
+
+A chain of components who all get a chance to process a command or a query,
+optionally having default processing implementation and an ability to terminate
+the processing chain.
+"""
