45d9572761d277b04bb95388d6b336b9f6671081
PyExplorer <stgmont@gmail.com>
2022-05-02 23:08:16 +0300

    [chain of responsibility] fix tests

--- tests/test_chain_of_responsibility.py
+++ tests/test_chain_of_responsibility.py
@@ -1,7 +1,9 @@
-"""Tests for classes with flyweight pattern."""
+"""Tests for classes with chain of responsibility pattern."""
 from src.design_patterns_in_python.chain_of_responsibility import Game
 from src.design_patterns_in_python.chain_of_responsibility import Goblin
 from src.design_patterns_in_python.chain_of_responsibility import GoblinKing
+from src.design_patterns_in_python.chain_of_responsibility import Query
+from src.design_patterns_in_python.chain_of_responsibility import WhatToQuery
 
 
 class TestGoblin:
@@ -13,6 +15,22 @@ class TestGoblin:
         assert goblin.attack == 1
         assert goblin.defense == 1
 
+    def test_str_goblin(self):  # noqa: D102
+        game = Game("Goblin fight")
+        goblin = Goblin("Goblin 1", game)
+        assert str(goblin) == (
+            "Goblin 1 in (Game: Goblin fight with number of creatures: 0) with attack: 1 and defence: 1"
+        )
+
+    def test_query(self):  # noqa: D102
+        game = Game("Goblin fight")
+        goblin_1 = Goblin("Goblin 1", game)
+        goblin_2 = Goblin("Goblin 2", game)
+        query = Query(1, WhatToQuery.DEFENSE)
+        assert query.value == 1
+        goblin_1.query(goblin_2, query)
+        assert query.value == 2
+
 
 class TestGoblinKing:
     """Tests for King Goblin."""
@@ -23,9 +41,30 @@ class TestGoblinKing:
         assert goblin.attack == 3
         assert goblin.defense == 3
 
+    def test_query_increasing(self):  # noqa: D102
+        game = Game("Goblin fight")
+        goblin_1 = GoblinKing("Goblin King", game)
+        goblin_2 = Goblin("Goblin 2", game)
+        query = Query(1, WhatToQuery.ATTACK)
+        assert query.value == 1
+        goblin_1.query(goblin_2, query)
+        assert query.value == 2
+
+    def test_query_no_change(self):  # noqa: D102
+        game = Game("Goblin fight")
+        goblin_1 = GoblinKing("Goblin King", game)
+        query = Query(1, WhatToQuery.DEFENSE)
+        assert query.value == 1
+        goblin_1.query(goblin_1, query)
+        assert query.value == 1
+
 
 class TestGame:
     """Tests for the pattern."""
+
+    def test_empty_game(self):  # noqa: D102
+        game = Game("Goblin fight")
+        assert str(game) == "(Game: Goblin fight with number of creatures: 0)"
 
     def test_pattern(self):  # noqa: D102
         game = Game("Goblin fight")
