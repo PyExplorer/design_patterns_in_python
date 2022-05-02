084052cce4a25e629c1992e952d964cd96527cb1
PyExplorer <stgmont@gmail.com>
2022-05-02 21:57:00 +0300

    [chain of responsibility] empty tests

---
+++ tests/test_chain_of_responsibility.py
@@ -0,0 +1,31 @@
+"""Tests for classes with flyweight pattern."""
+import pytest
+from src.design_patterns_in_python.chain_of_responsibility import Creature
+from src.design_patterns_in_python.chain_of_responsibility import Goblin
+from src.design_patterns_in_python.chain_of_responsibility import GoblinKing
+from src.design_patterns_in_python.chain_of_responsibility import Query
+from src.design_patterns_in_python.chain_of_responsibility import Game
+
+
+class TestGoblin:
+    """Tests for Goblin."""
+
+    pass
+
+
+class TestGoblinKing:
+    """Tests for King Goblin."""
+
+    pass
+
+
+class TestQuery:
+    """Tests for Query."""
+
+    pass
+
+
+class TestGame:
+    """Tests for Game."""
+
+    pass
