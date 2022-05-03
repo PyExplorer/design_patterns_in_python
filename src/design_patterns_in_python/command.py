85cc0996db2b2e56986d4a0083c429989de61f59
PyExplorer <stgmont@gmail.com>
2022-05-03 18:43:07 +0300

    [command] final

--- src/design_patterns_in_python/command.py
+++ src/design_patterns_in_python/command.py
@@ -8,27 +8,64 @@ from __future__ import annotations  # pragma: no cover
 from enum import Enum
 
 
-class Command:
-    class Action(Enum):
-        """Actions enumerator."""
-        DEPOSIT = 0
-        WITHDRAW = 1
+class Action(Enum):
+    """Actions enumerator."""
 
-    def __init__(self, action, amount):
+    DEPOSIT = 0
+    WITHDRAW = 1
+
+
+class Command:
+    """Single command."""
+
+    def __init__(self, action: Action, amount: float) -> None:  # noqa: D107
         self.action = action
         self.amount = amount
         self.success = False
 
 
 class Account:
-    def __init__(self, balance=0):
-        self.balance = balance
+    """Account."""
 
-    def process(self, command):
-        if command.action == Command.Action.DEPOSIT:
+    def __init__(self, balance: float = 0) -> None:  # noqa: D107
+        self.balance = balance
+        self.last_command = False
+
+    def process(self, command: Command) -> None:  # noqa: D102
+        if command.action == Action.DEPOSIT:
             self.balance += command.amount
             command.success = True
-        elif command.action == Command.Action.WITHDRAW:
+        elif command.action == Action.WITHDRAW:
             command.success = self.balance >= command.amount
             if command.success:
                 self.balance -= command.amount
+        self.last_command = command.success
+
+    def __str__(self) -> str:  # noqa: D105
+        return f"Balance: {self.balance}, last command was successful: {self.last_command}"
+
+
+def main():  # noqa: D103, pragma: no cover
+    """Task.
+
+    Implement the method to process different account commands with rules:
+    1. Success indicates whether the operation was successful
+    2.You can only withdraw money if you have enough in your account
+    """
+    a = Account()
+
+    cmd = Command(Action.DEPOSIT, 100)
+    a.process(cmd)
+    print(a)
+
+    cmd = Command(Action.WITHDRAW, 50)
+    a.process(cmd)
+    print(a)
+
+    cmd.amount = 150
+    a.process(cmd)
+    print(a)
+
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
