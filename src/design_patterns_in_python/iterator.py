0a14943ea3a36d47d9198c6d315f8823fd991577
PyExplorer <stgmont@gmail.com>
2022-05-10 23:21:08 +0300

    [iterator] fix mypy

--- src/design_patterns_in_python/iterator.py
+++ src/design_patterns_in_python/iterator.py
@@ -2,23 +2,26 @@
 
 An object that facilitates the traversal of data structure.
 """
+from __future__ import annotations  # pragma: no cover
+
+from typing import Generator
 
 
 class Node:
     """Class for Node."""
-    def __init__(self, value, left=None, right=None):
+
+    def __init__(self, value: str, left: Node = None, right: Node = None) -> None:  # noqa: D107
         self.right = right
         self.left = left
         self.value = value
-
         self.parent = None
 
-        if left:
+        if self.left:
             self.left.parent = self
-        if right:
+        if self.right:
             self.right.parent = self
 
-    def traverse_preorder(self):  # noqa: D102
+    def traverse_preorder(self) -> Generator[str, None, None]:  # noqa: D102
         yield self.value
         if self.left:
             yield from self.left.traverse_preorder()
